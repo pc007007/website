6a055cee3c981b3d7afb6361c5ab8a24dc674b3a
pc007007 <pc19900804@gmail.com>
2017-09-29 22:28:33 +0800

    base 5

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.contrib import admin
 
-class BlogsPost(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length = 150)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
     title = models.CharField(max_length = 150)
     author = models.CharField(max_length=150)
     timestamp = models.DateTimeField()
     content = models.TextField()
-    category =models.CharField(max_length = 150)
+    category = models.ForeignKey(Category, null=True, blank=True)
 
-class BlogPostAdmin(admin.ModelAdmin):
+    def __str__(self):
+        return self.title
+
+
+class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'timestamp')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
-admin.site.register(BlogsPost, BlogPostAdmin)
-
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category,CategoryAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from blog.models import BlogsPost
+from blog.models import Post, Category
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 # Create your views here.
 def index(request):
     #return HttpResponse("<h1>This is my blog homepage</h1>")
-    postList = BlogsPost.objects.order_by('-timestamp')
-    return render_to_response('index.html', {'postList': postList})
+    postList = Post.objects.order_by('-timestamp')
+    count = {
+        'post' : Post.objects.count(),
+        'category' : Category.objects.count()
+    }
+    return render_to_response('index.html', {'postList': postList, 'count' : count})
 
 def post(request,time,title):
-    blog_list = BlogsPost.objects.all()
-    return render_to_response('post.html', {'blog_list': blog_list, 'time':time, 'title':title})
+    blog_list = Post.objects.all()
+    count = {
+        'post' : Post.objects.count(),
+        'category' : Category.objects.count()
+    }
+    return render_to_response('post.html', {'blog_list': blog_list, 'time':time, 'title':title, 'count' : count})
